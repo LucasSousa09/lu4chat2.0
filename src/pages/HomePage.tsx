545fae783d11ff7d192feb24fd51e15834ea034e
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-20 16:59:35 -0300

    Add homepage elements and styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,20 @@
+import styles from './HomePage.module.css'
+
+import heroImg from '../assets/hero-image.jpg'
+
 export function HomePage(){
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className={styles.homepageContainer}>
+                <img src={heroImg} alt="" />
+                <div>
+                    <span>Lu4chat</span>
+                    <span className={styles.highlighted}>O novo aplicativo</span>
+                    <span>de conversas</span>
+                    <span className={styles.highlighted + " " + styles.inline}>on-line</span>
+                    <span className={styles.inline}>para você</span>
+
+                    <a href="">Conecte-se ao futuro</a>
+                </div>
         </div>
     )
 }
