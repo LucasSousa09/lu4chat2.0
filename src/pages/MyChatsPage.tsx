7636e36aec4763e535c062db8d43c3bd4bc8bdf4
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-28 18:03:39 -0300

    Update MyChatsPage

--- src/pages/MyChatsPage.tsx
+++ src/pages/MyChatsPage.tsx
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { MyChatsSidebar } from "../components/MyChatsSidebar";
+import { MyChatsMessages } from "../components/MyChatsMessages";
+import { MyChatsMessageSender } from "../components/MyChatsMessageSender";
+
+import styles from './MyChatsPage.module.css'
+
+import chatsDB from '../../mockDB.json'
+
+type ChatRoom = {
+    id: string; 
+    name: string; 
+    description: string; 
+    type: string; 
+    allowedUsersIds: string[]; 
+    messagesId: string;
+}
+
 export function MyChatsPage(){
+    const { chatId } = useParams()
+
+    const [ chatRooms, setChatRooms ] = useState<ChatRoom[]>([])
+
+    useEffect(() => {
+        setChatRooms(Object.values(chatsDB.rooms))
+    },[])
+
     return (
-        <div>
-            <h1>MyChats Page</h1>
+        <div className={styles.myChatsContainer}>
+            <MyChatsSidebar chatRooms={
+                chatRooms.map(rooms =>({
+                    id: rooms.id,
+                    roomName: rooms.name,
+                    roomType: rooms.type,
+                    roomDescription: rooms.description,
+                    enterRoom: chatId
+                }))
+            } />
+            <div className={styles.messagesContainer}>
+                {
+                    chatId !== undefined && (
+                        <>
+                            <MyChatsMessages 
+                                chatName={chatsDB.rooms[chatId].name} 
+                                roomMessagesId={chatsDB.rooms[chatId].messagesId}
+                                />
+                            <MyChatsMessageSender />
+                        </>
+                    )
+                }
+            </div>
         </div>
     )
 }
