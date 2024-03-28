5521373e1c603243707d5a02d7bbc16d0b0b0a10
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-28 17:58:45 -0300

    Add conditional elements rendering when signedUp on Header, and signout function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,40 @@
+import { useContext } from 'react'
+import { signOut } from 'firebase/auth'
 import { ListDashes } from '@phosphor-icons/react'
+
+import { auth } from '../../libs/firebase-config'
 
 import { NavLink } from "../NavLink"
 
-import logoImg from '../../assets/logo_white.png'
-import logoMini from '../../assets/logo_mini.png'
+import logoImg from "../../assets/logo_white.png"
+import logoMini from "../../assets/logo_mini.png"
 
 import styles from "./Header.module.css"
 
+import { UserContext } from "../../contexts/UserContext"
+import { useNavigate } from 'react-router-dom'
 
 type HeaderProps = {
     openSidebar: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export function Header({ openSidebar }: HeaderProps ){
+    const { user } = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    async function logout(){
+        try{
+            await signOut(auth)
+
+            navigate("/login")
+        }
+        catch(err){
+            console.error(err)
+        }
+
+    }
+
     return (
         <header className={styles.header}>
             <button 
@@ -30,8 +52,16 @@ export function Header({ openSidebar }: HeaderProps ){
                     <NavLink linkTo="/my-chats">Conversas</NavLink>
                     <NavLink linkTo="/public-chats">Salas Públicas</NavLink>
                     <div className={styles.userLinks}>
-                        <NavLink linkTo="/register">Criar Conta</NavLink>
-                        <NavLink linkTo="/login">Login</NavLink>
+                        {
+                            user?.email ? (
+                                <button onClick={logout} className={styles.logoutButton}>Deslogar</button>
+                            ) : (
+                                <>
+                                    <NavLink linkTo="/register">Criar Conta</NavLink>
+                                    <NavLink linkTo="/login">Login</NavLink>        
+                                </>     
+                            )
+                        }
                     </div>
                 </div>
             </nav>
