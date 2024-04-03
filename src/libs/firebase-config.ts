a150c7a2ac8fefcbf349ef718fca9b600268c118
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-03 13:27:12 -0300

    Update firebase config

--- src/libs/firebase-config.ts
+++ src/libs/firebase-config.ts
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://lu4chat-default-rtdb.firebaseio.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
@@ -18,6 +17,7 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore(app)
 const auth = getAuth(app)
+
 const googleProvider = new GoogleAuthProvider()
 
 export { app, db, auth, googleProvider }
