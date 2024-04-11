1fed60e29e4774e7a2d0b51811c3a93c744ed7b2
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-11 16:44:00 -0300

    Fix roomType bug & add leaveRoom function

--- src/components/ChatRoomInfo/index.tsx
+++ src/components/ChatRoomInfo/index.tsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { api } from "../../libs/axios";
 import styles from "./ChatRoomInfo.module.css"
+import { UserContext } from "../../contexts/UserContext";
+import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
 
 type ChatRoomInfoProps = {
     id: string;
@@ -16,6 +21,25 @@ export function ChatRoomInfo({
     roomDescription,
     enterRoom = undefined,
 }: ChatRoomInfoProps) {
+    const { user } = useContext(UserContext)
+    const navigate = useNavigate()
+
+
+    async function leaveRoom(){
+        if(user){
+            const res = await api.delete(`/exit-room/${user.uid}/${id}`)
+
+            if(res.status === 200){
+                navigate(0)
+                return
+            }
+            toast.error("Algo de errado aconteceu tente novamente mais tarde")
+
+        }
+        else{
+            console.log("Faça login antes de tentar excluir uma conta")
+        }
+    }
 
     return (
         <div className={`${styles.chatRoomInfoContainer} ${enterRoom === id && styles.currentRoom}`}>
@@ -24,7 +48,7 @@ export function ChatRoomInfo({
                    {roomName}
                 </strong>
                 <span>
-                    {roomType  === "private" ? "Pública" : "Privada"}
+                    {roomType  === "private" ? "Privada" : "Pública"}
                 </span>
             </header>
             <div>
@@ -33,11 +57,11 @@ export function ChatRoomInfo({
                 </span>
                 {
                     enterRoom !== id ? (
-                        <a href={`/my-chats/${id}`}>
+                        <Link to={`/my-chats/${id}`}>
                             Entrar                    
-                        </a>
+                        </Link>
                     ) : (
-                        <button>
+                        <button onClick={leaveRoom}>
                             Sair
                         </button>
                     )
