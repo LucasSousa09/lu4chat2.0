df48e79c8b472c2cbd049a051913228f4cba3b2d
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-11 16:57:04 -0300

    Get dinamic userRooms info

--- src/pages/MyChatsPage.tsx
+++ src/pages/MyChatsPage.tsx
@@ -1,31 +1,42 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { MyChatsSidebar } from "../components/MyChatsSidebar";
 import { MyChatsMessages } from "../components/MyChatsMessages";
 import { MyChatsMessageSender } from "../components/MyChatsMessageSender";
 
+import { api } from "../libs/axios";
+
 import styles from './MyChatsPage.module.css'
 
-import chatsDB from '../../mockDB.json'
+import { UserContext } from "../contexts/UserContext";
 
 type ChatRoom = {
     id: string; 
     name: string; 
-    description: string; 
     type: string; 
-    allowedUsersIds: string[]; 
-    messagesId: string;
+    description: string; 
 }
 
 export function MyChatsPage(){
+    const { user } = useContext(UserContext)
+
     const { chatId } = useParams()
 
     const [ chatRooms, setChatRooms ] = useState<ChatRoom[]>([])
 
     useEffect(() => {
-        setChatRooms(Object.values(chatsDB.rooms))
-    },[])
+        async function getUserRooms(){
+            if(user){
+                const { data: userRoomsData }: {data: {myRooms: ChatRoom[]}} = await api.post(`/get-user-rooms`, {userId: user.uid})
+                setChatRooms(userRoomsData.myRooms)
+            }
+
+            return
+        }
+
+        getUserRooms()
+    },[user])
 
     return (
         <div className={styles.myChatsContainer}>
@@ -42,11 +53,11 @@ export function MyChatsPage(){
                 {
                     chatId !== undefined && (
                         <>
-                            <MyChatsMessages 
+                            {/* <MyChatsMessages 
                                 chatName={chatsDB.rooms[chatId].name} 
                                 roomMessagesId={chatsDB.rooms[chatId].messagesId}
                                 />
-                            <MyChatsMessageSender />
+                            <MyChatsMessageSender /> */}
                         </>
                     )
                 }
