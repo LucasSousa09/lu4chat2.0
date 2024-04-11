8a777d4c6a9311ea4b3d86a2b927a27dc60c6e7c
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-11 16:54:11 -0300

    Update Layout context providers

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,27 +1,35 @@
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.min.css"
-import { useState } from "react";
-import { Outlet } from "react-router-dom" 
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import { Header } from "../components/Header";
 import { MobileSidebar } from "../components/MobileSidebar";
+import { EnterRoomModal } from "../components/Modal/EnterRoomModal";
+import { CreateRoomModal } from "../components/Modal/CreateRoomModal";
 
-import styles from './Layout.module.css'
 import { UserContextProvider } from "../contexts/UserContext";
 
-export function Layout(){
-    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false)
+import styles from './Layout.module.css'
+import { ModalContextProvider } from "../contexts/ModalContext";
+import { SidebarContextProvider } from "../contexts/SidebarContext";
 
+export function Layout(){  
     return (
         <UserContextProvider>
-            <div className={styles.mainContainer} >
-                <ToastContainer position="bottom-right" theme="light"/>
-                <div className={styles.contentContainer}>
-                    <Header openSidebar={setIsSidebarOpen} />
-                    <MobileSidebar isSidebarOpen={isSidebarOpen} closeSidebar={setIsSidebarOpen}/>
-                    <Outlet />
-                </div>
-            </div>
+            <ModalContextProvider>
+                <SidebarContextProvider>
+                    <div className={styles.mainContainer} >
+                        <ToastContainer position="bottom-right" theme="light"/>
+                        <div className={styles.contentContainer}>
+                            <Header />    
+                            <EnterRoomModal />
+                            <CreateRoomModal />
+                            <MobileSidebar />
+                            <Outlet />
+                        </div>
+                    </div>
+                </SidebarContextProvider>
+            </ModalContextProvider>
         </UserContextProvider>
     )
 }
