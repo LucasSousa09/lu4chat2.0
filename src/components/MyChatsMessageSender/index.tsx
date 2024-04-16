5fc3b2a5ecce07f2aba17d728992807a5eb62b27
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-16 13:55:34 -0300

    Send messages and update the realtimeDB

--- src/components/MyChatsMessageSender/index.tsx
+++ src/components/MyChatsMessageSender/index.tsx
@@ -1,12 +1,55 @@
 import styles from './index.module.css'
 
+import { toast } from 'react-toastify'
+import { FormEvent, useState } from 'react'
 import { PaperPlaneRight } from '@phosphor-icons/react'
 
-export function MyChatsMessageSender(){
+import { api } from '../../libs/axios'
+
+type MyChatsMessageSenderProps = {
+    chatId: string,
+    senderId: string,
+    senderName: string
+}
+
+export function MyChatsMessageSender({chatId, senderId, senderName}: MyChatsMessageSenderProps){
+    const [sendingMessage, setSendingMessage] = useState({
+        message: "",
+        isSending: false
+    }) 
+    
+    async function handleMessageSenderSubmit(evt: FormEvent){
+        evt.preventDefault()
+
+        if(sendingMessage.message.trim() === ""){
+            toast.error("Escreva alguma coisa antes de enviar uma mensagem!")
+            return 
+        }
+
+        setSendingMessage(state => ({...state, isSending: true}))
+
+        const res = await api.post('/send-message', {message: sendingMessage.message, chatId, senderId, senderName })
+
+        if(res.status === 201){
+            setSendingMessage(() => ({message: "", isSending: false}))
+        }
+        else{
+            console.error("Algo de errado ocorreu tente novamente mais tarde!")
+            setSendingMessage(state => ({...state, isSending: false}))
+        }
+    }
+
     return (
-        <form className={styles.messageSenderContainer}>
-            <input type="text" />
-            <button type="submit">
+        <form onSubmit={handleMessageSenderSubmit} className={styles.messageSenderContainer}>
+            <input
+                id='message' 
+                type="text" 
+                value={sendingMessage.message} 
+                onChange={(evt) => setSendingMessage(state => {
+                                       return {...state, message: evt.target.value}
+                                   }
+            )}/>
+            <button type="submit" disabled={sendingMessage.isSending}>
                 Enviar
                 <PaperPlaneRight weight="fill" size={20}/>
             </button>
